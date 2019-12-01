c4edc5a929f7c2812a20faf739ce1cf4bb4b3960
Michael Jaros <michael.jaros@jarosoft.at>
2019-12-01 22:36:55 +0100

    add AJAX api test page to web part of the app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,96 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <title>Wordweb API Test</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+<script>
+    function ajaxError (jqXHR, textStatus, errorThrown) {
+        console.log('error: ' + textStatus);
+        return;
+    }
 
-        <title>Laravel</title>
+    function getWords() {
+        $.ajax({
+            url: '/api/words',
+            type: 'GET',
+            success: function (data) {
+                var allWords=[] ;
+                $("#words").empty();
+                for(var i = 0 ; i < data.length; ++i) {
+                    allWords.push(data[i].name);
+                    $("#words").append('<li>'+data[i].id+': '+data[i].name+'</li>');
+                }
+                $("#words").val(allWords.join(', '));
+            },
+            error: ajaxError,
+        });
+    }
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    function addWord() {
+        $.ajax({
+            url: '/api/words',
+            type: 'POST',
+            data: {
+                name: $("#post-name").val(),
+            },
+            success: function (data) {
+                getWords();
+            },
+            error: ajaxError,
+        });
+        $("#post-name").val('' );
+    }
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    function changeWord() {
+        $.ajax({
+            url: '/api/words/'+$("#put-id").val(),
+            type: 'PUT',
+            data: {
+                name: $("#put-name").val(),
+            },
+            success: function (data) {
+                getWords();
+            },
+            error: ajaxError,
+        });
+        $("#put-name").val('' );
+    }
 
-            .full-height {
-                height: 100vh;
-            }
+    function deleteWord() {
+        $.ajax({
+            url: '/api/words/'+$("#delete-id").val(),
+            type: 'DELETE',
+            success: function (data) {
+                getWords();
+            },
+            error: ajaxError,
+        });
+        $("#delete-id").val('' );
+    }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    $(document).ready(function() {
+        getWords();
+    });
 
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
+</script>
+<h1>Wordweb API test</h1>
+<div>
+    <h2>GET</h2>
+    <ul id="words" style="height:100px; width: 200px; overflow-y: scroll;">
+    </ul>
+    <h2>POST</h2>
+    <p><input type="text" id="post-name" placeholder="name"></p>
+    <p><button type="button" onclick="addWord()">POST</button></p>
+    <h2>PUT</h2>
+    <p><input type="text" id="put-id" placeholder="id"></p>
+    <p><input type="text" id="put-name" placeholder="name"></p>
+    <p><button type="button" onclick="changeWord()">PUT</button></p>
+    <h2>DELETE</h2>
+    <p><input type="text" id="delete-id" placeholder="id"></p>
+    <p><button type="button" onclick="deleteWord()">DELETE</button></p>
+</div>
+</body>
 </html>
