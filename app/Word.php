3babdad0a7a35e18e5125ff5d9b7d636994e323f
Michael Jaros <michael.jaros@jarosoft.at>
2019-12-02 11:50:14 +0100

    synonyms table and relation

--- app/Word.php
+++ app/Word.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Word extends Model
 {
-    //
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function synonyms() {
+        return $this->belongsToMany(
+            'App\Word',
+            'synonyms',
+            'word_a_id',
+            'word_b_id'
+        );
+    }
+
+    public function setSynonym(Word $otherWord) {
+        $this->synonyms()->save($otherWord);
+        $otherWord->synonyms()->save($this);
+    }
+
+    public function isSynonym(Word $otherWord) : bool { }
 }
---
+++ database/migrations/2019_12_02_094834_create_synonyms_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSynonymsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('synonyms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('word_a_id');
+            $table->foreign('word_a_id')
+                ->references('id')
+                ->on('words')
+                ->onDelete('cascade');
+            $table->unsignedBigInteger('word_b_id');
+            $table->foreign('word_b_id')
+                ->references('id')
+                ->on('words')
+                ->onDelete('cascade');
+            $table->unique(['word_a_id', 'word_b_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('synonyms');
+    }
+}
